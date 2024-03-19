08766770b21f41ab6b435b5c2593ced04cfb1f33
Schmelzy <schmelzyy@gmail.com>
2024-03-19 23:33:38 +0200

    Added new class for the soil moisture sensor.
    Signed-off-by: Tugay Hasan <schmelzyy@gmail.com>

---
+++ soil_moisture_sensor.py
@@ -0,0 +1,34 @@
+import spidev
+
+class SoilMoistureSensor:
+    def __init__(self, spi_channel):
+        self.spi = spidev.SpiDev(0, spi_channel)
+        self.spi.max_speed_hz = 1200000
+
+    def get_adc(self, channel):
+        # Make sure ADC channel is 0 or 1
+        if channel != 0:
+            channel = 1
+
+        # Construct SPI message
+        msg = 0b11
+        msg = ((msg << 1) + channel) << 5
+        msg = [msg, 0b00000000]
+        reply = self.spi.xfer2(msg)
+
+        # Construct single integer out of the reply (2 bytes)
+        adc = 0
+        for n in reply:
+            adc = (adc << 8) + n
+
+        # Last bit (0) is not part of ADC value, shift to remove it
+        adc = adc >> 1
+
+        # Calculate voltage from ADC value
+        # considering the soil moisture sensor is working at 5V
+        voltage = (5 * adc) / 1024
+
+        return voltage
+
+    def valmap(value, istart, istop, ostart, ostop):
+        return ostart + (ostop - ostart) * ((value - istart) / (istop - istart))
